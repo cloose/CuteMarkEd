c8076ef652125e10cd705b283f8aac25ec9c597d
Christian Loose <christian.loose@hamburg.de>
2013-04-08 20:44:02 +0200

    Add key shortcut (Ctrl + Left Arrow) to remove header mark (#)

--- markdowneditor.cpp
+++ markdowneditor.cpp
@@ -9,19 +9,23 @@ MarkdownEditor::MarkdownEditor(QWidget *parent) :
 
 void MarkdownEditor::keyPressEvent(QKeyEvent *e)
 {
+    // Ctrl+Right Arrow
     if ((e->key() == Qt::Key_Right) && (e->modifiers() & Qt::ControlModifier)) {
+        // move cursor to start of line
         QTextCursor cursor = textCursor();
         cursor.beginEditBlock();
         cursor.movePosition(QTextCursor::StartOfLine);
 
+        // search for last heading mark (#)
         int pos = cursor.position();
         while ('#' == cursor.document()->characterAt(pos++))
             ;
+
         if ((pos - cursor.position()) < 7) {
             cursor.insertText("#");
-            if (' ' != cursor.document()->characterAt(pos))
+            if (' ' != cursor.document()->characterAt(pos)) {
                 cursor.insertText(" ");
-            else {
+            } else {
                 int pos = cursor.position();
                 QTextDocument *doc = document();
                 while (' ' == doc->characterAt(pos))
@@ -35,6 +39,26 @@ void MarkdownEditor::keyPressEvent(QKeyEvent *e)
             }
         }
         cursor.endEditBlock();
+
+        e->accept();
+        return;
+    }
+
+    // Ctrl+Left Arrow
+    if ((e->key() == Qt::Key_Left) && (e->modifiers() & Qt::ControlModifier)) {
+        QTextCursor cursor = textCursor();
+        cursor.movePosition(QTextCursor::StartOfLine);
+
+        if ('#' == cursor.document()->characterAt(cursor.position())) {
+            cursor.beginEditBlock();
+            cursor.movePosition(QTextCursor::Right, QTextCursor::KeepAnchor);
+            cursor.removeSelectedText();
+            if (' ' == cursor.document()->characterAt(cursor.position())) {
+                cursor.movePosition(QTextCursor::Right, QTextCursor::KeepAnchor);
+                cursor.removeSelectedText();
+            }
+            cursor.endEditBlock();
+        }
 
         e->accept();
         return;
