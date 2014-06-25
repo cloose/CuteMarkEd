cd85a699528735e926387964938f333a32f9bb3a
Christian Loose <christian.loose@hamburg.de>
2014-06-25 14:38:21 +0200

    COVERITY-14276/14277: unchecked dynamic_cast
    Fix possible null pointer dereferences caused by unchecked dynamic_cast
    calls in DiscountMarkdownConverter class.

--- app-static/converter/discountmarkdownconverter.cpp
+++ app-static/converter/discountmarkdownconverter.cpp
@@ -74,7 +74,7 @@ QString DiscountMarkdownConverter::renderAsHtml(MarkdownDocument *document)
     if (document) {
         DiscountMarkdownDocument *doc = dynamic_cast<DiscountMarkdownDocument*>(document);
 
-        if (doc->document()) {
+        if (doc && doc->document()) {
             char *out;
             mkd_document(doc->document(), &out);
 
@@ -92,7 +92,7 @@ QString DiscountMarkdownConverter::renderAsTableOfContents(MarkdownDocument *doc
     if (document) {
         DiscountMarkdownDocument *doc = dynamic_cast<DiscountMarkdownDocument*>(document);
 
-        if (doc->document()) {
+        if (doc && doc->document()) {
             // generate table of contents
             char *out;
             mkd_toc(doc->document(), &out);
