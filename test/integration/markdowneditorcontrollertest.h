2e0b533008500eb2caecb8cca869e56339f831b6
Christian Loose <christian.loose@hamburg.de>
2015-01-19 21:22:05 +0100

    [MVC] Skeleton of a controller class for MarkdownEditor

---
+++ test/integration/markdowneditorcontrollertest.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2015 Christian Loose <christian.loose@hamburg.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef MARKDOWNEDITORCONTROLLERTEST_H
+#define MARKDOWNEDITORCONTROLLERTEST_H
+
+#include <QObject>
+class QPlainTextEdit;
+class MarkdownEditorController;
+
+class MarkdownEditorControllerTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+
+private:
+    QPlainTextEdit *editorView;
+    MarkdownEditorController *controller;
+};
+
+#endif // MARKDOWNEDITORCONTROLLERTEST_H
+
+
