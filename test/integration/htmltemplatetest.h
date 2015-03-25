b536ab06998a33cabef75acb8dab4b513bda5cd3
Christian Loose <christian.loose@hamburg.de>
2015-03-25 08:13:11 +0100

    [#215] New integration tests for replacement of mermaid code sections

--- test/integration/htmltemplatetest.h
+++ test/integration/htmltemplatetest.h
@@ -1,20 +1,19 @@
-#pragma once
 /*
-* Copyright 2015 Christian Loose <christian.loose@hamburg.de>
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright 2015 Christian Loose <christian.loose@hamburg.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef HTMLTEMPLATETEST_H
 #define HTMLTEMPLATETEST_H
 
@@ -26,6 +25,8 @@ class HtmlTemplateTest : public QObject
 
 private slots:
 	void rendersContentInsideBodyTags();
+    void rendersMermaidGraphInsideCodeTags();
+    void replacesMermaidCodeTagsByDivTagsIfCodeHighlightingEnabled();
 };
 
 #endif // HTMLTEMPLATETEST_H
